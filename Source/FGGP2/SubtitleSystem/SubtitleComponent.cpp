8b7d4087531f319cec4bf19a080750f80a37f911
Martin Carlsson <Martin.Carlsson@futuregames.nu>
2020-08-27 14:40:27 +0200

    Revert "Revert "Moved interact interface to c++, changed interface behaviour. Implemented sound file for subtitles, changed subtitle system.,""
    This reverts commit 73459951766877429f5a2fe354eb4ddd5b30cb49.

---
+++ Source/FGGP2/SubtitleSystem/SubtitleComponent.cpp
@@ -0,0 +1,96 @@
+#include "SubtitleComponent.h"
+
+USubtitleComponent::USubtitleComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	
+	index = -1;
+
+}
+
+void USubtitleComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void USubtitleComponent::Clicked_Implementation()
+{
+	if (DataAsset == nullptr)
+	{
+		return;
+	}
+	if (DataAsset->texts.Num() > 0)
+	{
+		switch (DataAsset->texts[index].triggerType)
+		{
+		case ENextLineTrigger::OnClick:
+		{
+
+		}break;
+		case ENextLineTrigger::OnFixedDuration:
+		{
+			return;
+		}break;
+		case ENextLineTrigger::OnFixedDurationOrClick:
+		{
+			StopTimer();
+		}break;
+		case ENextLineTrigger::OnTriggerEnter:
+		{
+			return;
+		}break;
+		}
+	}
+
+	if (index == 0)
+	{
+		Start();
+	}
+	else
+	{
+		Next();
+	}
+}
+
+void USubtitleComponent::Next()
+{
+	index++;
+	if (index < DataAsset->texts.Num())
+	{
+		SubtitleEvent.Broadcast(ESubtitleEvent::SubtitleNext);
+	}
+	else
+	{
+		Ended();
+	}
+}	
+void USubtitleComponent::Start()
+{
+	SubtitleEvent.Broadcast(ESubtitleEvent::SubtitleStarted);
+}
+
+void USubtitleComponent::Ended()
+{
+	index = 0;
+	SubtitleEvent.Broadcast(ESubtitleEvent::SubtitleFinished);
+}
+
+FTextData USubtitleComponent::GetCurrentTextData()
+{
+	if (index >= 0)
+	{
+		return DataAsset->texts[index];
+	}
+	return FTextData();
+}
+
+void USubtitleComponent::StartTimer(float duration)
+{
+	GetWorld()->GetTimerManager().SetTimer(ShowNextSubtitleTimerHandle, this, &USubtitleComponent::Next, 0, false, duration);
+}
+
+void USubtitleComponent::StopTimer()
+{
+	GetWorld()->GetTimerManager().ClearTimer(ShowNextSubtitleTimerHandle);
+}
